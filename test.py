66d1537dd2a7fa49c939484adb62fefee29a2d04
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-31 10:42:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,37 @@
 import cv2
-import dlib
-import tensorflow as tf
 import numpy as np
 
-# Load the pre-trained face detection model from dlib
-detector = dlib.get_frontal_face_detector()
+# Load the pre-trained Caffe models for age and gender
+age_net = cv2.dnn.readNetFromCaffe("age_deploy.prototxt", "age_net.caffemodel")
+gender_net = cv2.dnn.readNetFromCaffe("gender_deploy.prototxt", "gender_net.caffemodel")
 
-# Load the pre-trained facial landmarks model from dlib
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+# Define the list of age ranges and gender labels
+age_ranges = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
+gender_labels = ["Male", "Female"]
 
-# Load the pre-trained age and gender models
-age_model = tf.keras.models.load_model("age_model.h5")
-gender_model = tf.keras.models.load_model("gender_model.h5")
+# Load the image using OpenCV
+image = cv2.imread("image.jpg")  # Replace with the path to your image
 
-def detect_face(image_path):
-    # Load the image using OpenCV
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Define a function to predict age and gender
+def predict_age_and_gender(image):
+    # Resize the image to the input size expected by the models
+    blob = cv2.dnn.blobFromImage(image, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
 
-    # Detect faces in the image
-    faces = detector(gray)
+    # Predict age
+    age_net.setInput(blob)
+    age_preds = age_net.forward()
+    age = age_ranges[np.argmax(age_preds)]
 
-    # Process each detected face
-    for face in faces:
-        # Get facial landmarks
-        landmarks = predictor(gray, face)
+    # Predict gender
+    gender_net.setInput(blob)
+    gender_preds = gender_net.forward()
+    gender = gender_labels[np.argmax(gender_preds)]
 
-        # Extract features from landmarks
-        features = []
-        for i in range(68):
-            x, y = landmarks.part(i).x, landmarks.part(i).y
-            features.append(x)
-            features.append(y)
+    return age, gender
 
-        # Convert the features to a NumPy array
-        features = np.array(features)
+# Get age and gender predictions
+age, gender = predict_age_and_gender(image)
 
-        # Resize the image for the age and gender models
-        face_img = cv2.resize(image[face.top():face.bottom(), face.left():face.right()], (224, 224))
-        face_img = np.expand_dims(face_img, axis=0)
-
-        # Predict age and gender
-        predicted_age = age_model.predict(face_img)[0]
-        predicted_gender = gender_model.predict(face_img)[0]
-
-        # Define the gender labels
-        gender_labels = ['Male', 'Female']
-
-        # Print the results
-        print(f"Age: {int(predicted_age)} years")
-        print(f"Gender: {gender_labels[np.argmax(predicted_gender)]}")
-
-if __name__ == "__main__":
-    image_path = "image.jpg"
-    detect_face(image_path)
+# Print the results
+print(f"Age: {age}")
+print(f"Gender: {gender}")
